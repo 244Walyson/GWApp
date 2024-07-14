c2aeee653c8934cc77fff0005e9afa2f0ce1fee5
walyson moises <walysonmoises1342@gmail.com>
2024-07-14 16:16:50 -0300

    fix api gateway

--- gateway-service/src/test/java/com/waly/api_gateway/ApiGatewayApplicationTests.java
+++ gateway-service/src/test/java/com/waly/api_gateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kanban.chat;
+package com.waly.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
